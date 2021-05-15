1f8df67b0028dce79f3529a2b2ad6b14ea9809d5
milka <milka@mil.com>
2021-05-15 22:45:17 +0300

    add name,question and answer

--- src/index.js
+++ src/index.js
@@ -2,21 +2,16 @@ import ReactDOM from "react-dom";
 ReactDOM.render(
     <table border="1" width="1000">
         <tr>
-            <td colSpan="2" align="center"><h1>Илон Маск</h1></td>
+            <td colSpan="2" align="center"><Name/></td>
         </tr>
-        <td><ImgIlon/>
-        <td width="600" valign="top"><i><b>Илон Рив Маск</b></i>-американский предприниматель, изобретатель, инженер и
-            миллиардер.
-            <br>
-                <br>
-                    <b> Дата и место рождения:</b> 28 июня 1971 г. (возраст 49 лет), Претория, ЮАР
-                    <br>
-                        <br>
-                            <b>Собственный капитал:</b> 90,8 миллиарда USD (2020 г.) Forbes
+        <td><ImgIlon/></td>
+        <td width="600" valign="top"><i><Question name="Илон Рив Маск"/></i> <Answer nameResult=" -американский предприниматель, изобретатель, инженер и
+            миллиардер."/>
+            <div><Question data="Дата и место рождения:"/> <Answer dataResult="28 июня 1971 г. (возраст 49 лет), Претория, ЮАР"/></div>
+            <div><Question money="Собственный капитал:"/> <Answer moneyResult="90,8 миллиарда USD (2020 г.) Forbes"/></div>
         </td>
-        </tr>
         <tr>
-            <td colSpan="2" align="center"><a href="#">(c)intocode</a></td>
+            <td colSpan="2" align="center"><Link/></td>
         </tr>
     </table>,
     document.getElementById('root'));
